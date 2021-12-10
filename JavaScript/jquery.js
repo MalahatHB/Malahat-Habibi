189f3fb50b80989e0d2e17fcc450cf5db5d161e7
MalahatHabibi <malahat.habibi2001@gmail.com>
2021-12-10 23:32:50 +0330

    Assignment 7 changes

---
+++ JavaScript/jquery.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    $("#menu").click(function () {
+        $("#menu-items").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#introduction").click(function () {
+        $("#introduction-paragraph").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#references").click(function () {
+        $("#references-links").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#morning-sunlight-video").click(function () {
+        $("#morning-sunlight-video-mp4").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#azadi-tower").click(function () {
+        $("#azadi-tower-img").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#milad-tower").click(function () {
+        $("#milad-tower-img").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#map-of-iran").click(function () {
+        $("#map-of-iran-img").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#location-and-subdivisions").click(function () {
+        $("#location-and-subdivisions-paragraph").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#more-information").click(function () {
+        $("#more-information-details").slideToggle("slow");
+    });
+});
